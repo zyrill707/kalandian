4e3b4e5bc922f24e7d1b61241974b6d15c3d1aff
testproject-testing-cf <56145975+zyrill707@users.noreply.github.com>
2024-03-14 22:47:53 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("on").addEventListener('click', function(){
+  document.body.style.backgroundColor = "white";
+})
+
+document.getElementById("off").addEventListener('click', function(){
+  document.body.style.backgroundColor = 'black'
+})
